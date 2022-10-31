3c2469613924bd85edc462c8c362c267ae213789
Arbee4ever <robin.baenecke@mail.de>
2022-10-31 09:43:31 +0100

    sorter impl

--- sorter.lua
+++ sorter.lua
@@ -1,4 +1,21 @@
-local chests = peripheral.getNames()
-for _, inv in pairs(chests) do
-	print(inv)
+local input = peripheral.find("minecraft:barrel")
+
+local misc = peripheral.find("minecraft:trapped_chest")
+local chests = { peripheral.find("minecraft:chest") }
+
+while true do
+	os.pullEvent("redstone")
+	if redstone.getInput("left") == true then
+		for inputSlot, inputItem in pairs(input.list()) do
+			for _, chest in pairs(chests) do
+				for slot, item in pairs(chest.list()) do
+					if inputItem.name == item.name then
+						chest.pullItems(peripheral.getName(input), inputSlot)
+						break
+					end
+				end
+			end
+			misc.pullItems(peripheral.getName(input), inputSlot)
+		end
+	end
 end
